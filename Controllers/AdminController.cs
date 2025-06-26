f19526a8127a75a2c46e7533b2e4200c815938dd
kt9897224 <157088494+kt9897224@users.noreply.github.com>
2025-06-26 13:22:47 +0700

    Layout

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PerfumeShop.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: AdminController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
